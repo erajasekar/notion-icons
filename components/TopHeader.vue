f0ef999c8b8cec5abac9f1a160b40eafb450f0ff
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-02 16:25:29 -0400

    First version of header-footer added

---
+++ components/TopHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="hero is-primary max-w-full border-b-4 border-yellow-700">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          Notion Icons
+        </h1>
+        <h2 class="italic text-lg text-yellow-800">
+          Tool to simplify using Icons in Notion
+        </h2>
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <nav class="tabs is-boxed is-fullwidth">
+        <div class="container">
+          <ul>
+            <li :class="isMenuActive('index')">
+              <a href="/">Home</a>
+            </li>
+            <li :class="isMenuActive('gallery')">
+              <nuxt-link to="/gallery">
+                Icons
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+
+import { Vue } from 'nuxt-property-decorator'
+export default class extends Vue {
+  isMenuActive (menuName: string) {
+    return this.$route.name === menuName ? 'is-active' : ''
+  }
+}
+
+</script>
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,74 +1,24 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
-  </div>
+  <section class="center site">
+    <TopHeader />
+    <div class="container main">
+      <nuxt />
+    </div>
+    <BottomFooter />
+  </section>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
+import { Component, Vue } from 'nuxt-property-decorator'
+import TopHeader from '@/components/TopHeader.vue'
+import BottomFooter from '@/components/BottomFooter.vue'
+
+@Component({
+  components: {
+    TopHeader,
+    BottomFooter
   }
+})
+export default class extends Vue {
 }
 </script>
