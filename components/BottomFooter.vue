702553f51cfd46a4a8414b341368736e0958c732
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-02 16:25:29 -0400

    First version of header-footer added

---
+++ components/BottomFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class=" mt-5 p-5 bg-red-900">
+    <div class="level font-bold text-yellow-200 text-xl">
+      <p class="level-item">
+        by &nbsp; <a href="https://twitter.com/erajasekar"> @erajasekar</a>.
+      </p>
+    </div>
+  </footer>
+</template>
