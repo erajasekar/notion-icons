e5e785e5af1001233a211e55df6747709e6eccae
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-03 00:06:35 -0400

    Got Data Parsing Working

--- gallery/config.ts
+++ gallery/config.ts
@@ -1,3 +1,36 @@
 import data from '../config/icons-config.json'
 
-console.log('RAJA ', data)
+interface IIconInfo {
+  name: string
+  tags: Array<string>
+}
+
+class IconInfo implements IIconInfo {
+  name: string
+  tags: Array<string>
+
+  constructor (name: any, tags: any) {
+    this.name = name
+    this.tags = tags
+  }
+}
+
+export class IconConfig {
+  url: string
+  size: string
+  style: string
+  icons : Array<IconInfo>
+
+  constructor () {
+    const icon8Data: any = data.config[0].icon8
+    this.url = icon8Data.url
+    this.size = icon8Data.size
+    this.style = icon8Data.style
+    this.icons = []
+    icon8Data.icons.forEach((ele: any) => {
+      this.icons.push(new IconInfo(ele.name, ele.tags))
+    })
+  }
+}
+const iconsData: IconConfig = new IconConfig()
+export default iconsData
