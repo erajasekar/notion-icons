b6e970c2b2996170bf0b26a2023cd1f87f82f123
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-02 22:45:01 -0400

    Added side content

---
+++ pages/icons.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="main-content columns">
+    <aside class="column is-2 section">
+      <p class="">
+        Filter and Customize
+      </p>
+    </aside>
+
+    <div class="container column is-10">
+      <p class="">
+        ICONS
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
