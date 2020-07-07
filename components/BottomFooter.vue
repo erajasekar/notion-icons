2824c01372191c7e10dff70fa7f41e75ede9a3ef
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-07 16:33:54 -0400

    Customize colors

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -2,7 +2,10 @@
   <footer class="mt-5 p-5 bg-indigo-900">
     <div class="level font-bold text-yellow-200 text-xl">
       <p class="level-item text-white">
-        by &nbsp; <a href="https://twitter.com/erajasekar" class="text-yellow-400 hover:text-yellow-600"> @erajasekar</a>.
+        Made by &nbsp; <a href="https://twitter.com/erajasekar" class="text-yellow-400 hover:text-yellow-600"> @erajasekar</a>.
+      </p>
+      <p class="level-item text-white">
+        Powered by  &nbsp; <a href="https://icons8.com/license" class="text-yellow-400 hover:text-yellow-600"> Icons8</a>.
       </p>
     </div>
   </footer>
