e9788f279a4a86179ce18d29beff87e747294d90
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-07 00:05:09 -0400

    Initial version hover icon display

--- pages/icons.vue
+++ pages/icons.vue
@@ -26,17 +26,29 @@
 
     <div class="container column is-10">
       <section :class="applyTheme">
-        <div class="columns is-mobile is-multiline tile is-ancestor">
-          <div v-for="(icon, index) in filteredIcons" :key="index" class="column is-one-fifth tile is-parent">
-            <div class="tile is-child box dark-card" @click="copyIcon(icon.name)">
-              <div v-if="isIconSelected(icon.name)">
-                Copied!
+        <div class="columns is-mobile is-multiline">
+          <div v-for="(icon, index) in filteredIcons" :key="index" class="column is-one-fifth">
+            <div class="icon-card" @click="copyIcon(icon.name)" @mouseover="iconHover(icon.name,true)" @mouseleave="iconHover(icon.name,false)">
+              <div class="has-text-centered pb-5 pt-5">
+                <p v-if="isIconSelected(icon.name)">
+                  Copied!
+                </p>
+                <b-icon
+                  v-else-if="isIconHover(icon.name)"
+                  icon="content-copy"
+                />
               </div>
-              <div class="title">
-                <img :ref="icon.name" height="50px" width="50px" :src="getImgUrl(icon.name)">
+              <div class="">
+                <div class="level">
+                  <div class="level-item has-text-centered">
+                    <figure class="image is-64x64">
+                      <img :ref="icon.name" :src="getImgUrl(icon.name)">
+                    </figure>
+                  </div>
+                </div>
               </div>
-              <footer class="subtitle">
-                <div class="">
+              <footer class="">
+                <div class="subtitle has-text-centered pt-5 pb-5">
                   {{ icon.name }}
                 </div>
               </footer>
@@ -65,6 +77,7 @@ export default class extends Vue {
   iconsData : IconConfig = new IconConfig()
   color = '#0000FF'
   selectedIcon = ''
+  hoverIcon = ''
   searchKeyword = ''
   isDarkMode = false
 
@@ -84,11 +97,23 @@ export default class extends Vue {
     return name === this.selectedIcon
   }
 
+  isIconHover (name: string) {
+    return name === this.hoverIcon
+  }
+
   copyIcon (iconName: string) {
     const firstImg = (this.$refs[iconName] as any)[0]
     this.$copyText(firstImg.src).then(() => {
       this.selectedIcon = iconName
     })
+  }
+
+  iconHover (iconName: string, hover: boolean) {
+    if (hover) {
+      this.hoverIcon = iconName
+    } else {
+      this.hoverIcon = ''
+    }
   }
 
   get applyTheme () {
@@ -116,16 +141,21 @@ export default class extends Vue {
     border: 10px;
     max-width: 100%;
   }*/
+
+  .is-horizontal-center {
+    justify-content: center;
+  }
+
   div.icon-card:hover{
-    box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-    color: #4a4a4a;
+    /*box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+    color: #4a4a4a;*/
+    background-color: whitesmoke;
   }
   div.dark-card:hover{
-    box-shadow: 5 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-    color: white;
+    background-color: #34393d;
   }
-  /* div.dark-card{
+  div.dark-card{
     background-color: #2F3437;
-    color: white;
-  } */
+    color: #f2f2f2;
+  }
 </style>
