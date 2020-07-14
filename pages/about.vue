670de6deddd141b2f95486b18c95d9bfb8da93cb
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-13 21:43:42 -0400

    Added skeleton of about page

--- pages/about.vue
+++ pages/about.vue
@@ -7,17 +7,77 @@
         </h1>
         <p class="text-lg">
           Are you a <a href="http://notion.so/" target="_blank"> Notion </a> user ?
-          Do you want to organize your notes with beautiful Icons ? You have landed at right website.
+          Do you want to organize your notes with beautiful Icons ? This is a tool for you!
         </p>
       </div>
       <div class="">
         <h1>
           Frequently Asked Questions
         </h1>
-        <p class="text-lg">
-          Are you a <a href="http://notion.so/" target="_blank"> Notion </a> user ?
-          Do you want to organize your notes with beautiful Icons ? You have landed at right website.
-        </p>
+        <div class="">
+          <h2>
+            I don't find the Icon I want, What should I do ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            I am not familiar with using Github pull request , Is there another way request Icons ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            How are current list of icons currated ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            How are current list of icons currated ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            Can I request other style of icons from icon8 icons or icons from other sites flaticon?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            Is it free to use ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            The is hosted at notion.erajasekar.com ? Will there any problem if tools move to different address ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
+        <div class="">
+          <h2>
+            I found some problems or something is not working, What should I do ?
+          </h2>
+          <p class="text-lg">
+            Answer
+          </p>
+        </div>
       </div>
     </section>
   </section>
