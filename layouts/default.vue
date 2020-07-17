b9ee93c4e0c9f4e95fd60a6e4f04fd4fad95c3b1
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-17 16:21:56 -0400

    Added contribution instructions

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,6 +25,17 @@ export default class Layout extends Vue {
 
 <style>
   @import url('https://fonts.googleapis.com/css?family=KoHo|Share+Tech+Mono|Roboto|Titillium+Web');
+
+  html {
+    font-size: 18px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
   body {
     font-family: 'Titillium Web', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
    'Helvetica Neue', Arial, sans-serif;
