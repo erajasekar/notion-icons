5f2fd2755a9773e6008975663bbbf516e22176de
Rajasekar Elango <e.rajasekar@gmail.com>
2020-07-17 12:12:33 -0400

    Added why in about page

--- pages/about.vue
+++ pages/about.vue
@@ -5,11 +5,31 @@
         <div class="hero-body">
           <div class="qa">
             <h1>
-              Why did I build this?
+              Why did I build Notion Icons?
             </h1>
-            <p class="">
-              Are you a <a href="http://notion.so/" target="_blank"> Notion </a> user ?
-              Do you want to organize your notes with beautiful Icons ? This is a tool for you!
+            <p>
+              I switched to Notion for note taking and have been loving it. I use the popular
+              <a href="https://youtu.be/FZIMlWLW4gU" target="_blank"> PARA </a> ( Projects, Areas, Resources, Archieve )
+              method to save resources I learn from all sources like blogs, videos , podcasts, books etc.
+              I like using colorful Icons in notes as it gives a quick visual context. Here is example from my Notion.
+            </p>
+            <figure class="image box">
+              <img class="card" width="70%" src="~assets/notions-related-icons.png" alt="Raja Notion Screen shot">
+            </figure>
+            <p>
+              Notions comes with only limited set of icons. We can get beautiful icons from many websites like
+              <a href="https://icons8.com/" target="_blank"> Icon8 </a> ,
+              <a href="https://www.flaticon.com/" target="_blank"> Flaticon </a> .
+              But it is painful to add icons from those websites to notion notes.  I need to first find the right icon from thousands of icons,
+              then customize and download to my computer. After that I can upload new icon to Notion.
+            </p>
+            <p>
+              I searched for better solution and found easy to use Icons from <a href="https://www.notion.vip/icons/" target="_blank"> notion.vip </a>,
+              but again it doesn't have all the Icons I want to use. But I like it's idea.
+            </p>
+            <p>
+              I also saw from reddit posts that other people are also looking for solution to this problem. So I decided to build this tool Myself
+              and share it with the Notion community.
             </p>
           </div>
           <hr>
@@ -28,14 +48,6 @@
             <div class="qa">
               <h3>
                 I am not familiar with using Github pull request , Is there another way request Icons ?
-              </h3>
-              <p class="text-lg">
-                Answer
-              </p>
-            </div>
-            <div class="qa">
-              <h3>
-                How are current list of icons currated ?
               </h3>
               <p class="text-lg">
                 Answer
@@ -101,6 +113,8 @@
   p {
     font-family: KoHo,sans-serif;
     font-size: x-large;
+    font-weight: 600;
+    color : #4a5568;
   }
 
   .qa {
@@ -110,4 +124,5 @@
   .card {
     box-shadow: 0 2px 2px 0 rgba(34, 41, 47, 0.05), 0 8px 8px 0 rgba(34, 41, 47, 0.05);
   }
+
 </style>
